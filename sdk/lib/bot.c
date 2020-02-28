8a03468900c533796d8dddc4147132d46f7553ca
wangao <1366463855@qq.com>
2020-02-28 09:18:26 +0800

    feat(sdk): add THUAI interface (credit to shiqing)

--- sdk/lib/bot.c
+++ sdk/lib/bot.c
@@ -7,7 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <time.h>
+#include <unistd.h>
 
 #define quq(__syscall, ...) _quq(#__syscall, __syscall(__VA_ARGS__))
 
@@ -38,14 +40,15 @@ static inline char tenacious_write(int fd, const char *buf, size_t len)
     return 0;
 }
 
-int bot_send_blob(int pipe, size_t len, const char *payload)
+/* Sends data prefixed with its 24-bit length to a file descriptor. */
+static int bot_send_blob(int pipe, size_t len, const char *payload)
 {
     if (len == 0) len = strlen(payload);
     if (len > 0xffffff) return BOT_ERR_TOOLONG;
     char len_buf[3] = {
-        len & 0xff,
-        (len >> 8) & 0xff,
-        (len >> 16) & 0xff
+        (char)(len & 0xff),
+        (char)((len >> 8) & 0xff),
+        (char)((len >> 16) & 0xff)
     };
     if (tenacious_write(pipe, len_buf, 3) < 0 ||
         tenacious_write(pipe, payload, len) < 0)
@@ -56,7 +59,13 @@ int bot_send_blob(int pipe, size_t len, const char *payload)
     return BOT_ERR_NONE;
 }
 
-char *bot_recv_blob(int pipe, size_t *o_len, int timeout)
+/*
+  Receives data from a file descriptor with a given timeout.
+  Returns a pointer to the data and stores the length in *o_len.
+  In case of errors, returns NULL, stores the error code (see above) in *o_len,
+  and prints related messages to stderr.
+ */
+static char *bot_recv_blob(int pipe, size_t *o_len, int timeout)
 {
     struct pollfd pfd = (struct pollfd){pipe, POLLIN, 0};
     char *ret = NULL;
@@ -161,13 +170,25 @@ const char *bot_strerr(int code)
     }
 }
 
-#define child_pause(__cp)   kill((__cp).pid, SIGSTOP)
-#define child_resume(__cp)  kill((__cp).pid, SIGCONT)
-#define child_kill(__cp)    kill((__cp).pid, SIGKILL)
+typedef struct _bot_player {
+    pid_t pid;
+    /* fd_send is the child's stdin, fd_recv is stdout
+       Parent writes to fd_send and reads from fd_recv */
+    int fd_send, fd_recv;
+    int fd_log;
+} bot_player;
 
-childproc child_create(const char *cmd, const char *log)
+#define bot_player_pause(__cp)   kill(-(__cp).pid, SIGUSR1)
+#define bot_player_resume(__cp)  kill(-(__cp).pid, SIGUSR2)
+
+/*
+  Creates the child.
+  Child processes are normally paused, but during `bot_player_recv()`
+  the process is resumed, and paused again after its response arrives.
+ */
+static bot_player bot_player_create(const char *cmd, const char *log)
 {
-    childproc ret;
+    bot_player ret;
     ret.pid = -1;
 
     int fd_send[2], fd_recv[2];
@@ -177,7 +198,7 @@ childproc child_create(const char *cmd, const char *log)
                        reported as "System Error" */
     }
 
-    int fd_log = open(log, O_WRONLY | O_CREAT, 0644);
+    int fd_log = open(log, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd_log == -1) {
         fprintf(stderr, "open(%s) failed with errno %d\n", log, errno);
         exit(1);
@@ -196,8 +217,8 @@ childproc child_create(const char *cmd, const char *log)
         dup2(fd_log, STDERR_FILENO);
         close(fd_send[1]);
         close(fd_recv[0]);
-        if (execl(cmd, cmd, NULL) != 0) {
-            if (execl("/bin/sh", "/bin/sh", "-c", cmd, NULL) != 0) {
+        if (execl(cmd, cmd, (char *)NULL) != 0) {
+            if (execl("/bin/sh", "/bin/sh", "-c", cmd, (char *)NULL) != 0) {
                 fprintf(stderr, "exec(%s) failed with errno %d\n", cmd, errno);
                 exit(1);
             }
@@ -209,28 +230,50 @@ childproc child_create(const char *cmd, const char *log)
         ret.pid = pid;
         ret.fd_send = fd_send[1];
         ret.fd_recv = fd_recv[0];
-        child_pause(ret);
+        bot_player_pause(ret);
     }
 
     return ret;
 }
 
-void child_finish(childproc proc)
+int num_players;
+static bot_player *players;
+
+int bot_player_init(int argc, char *const argv[])
 {
-    fsync(proc.fd_log);
-    child_kill(proc);
+    int n = (argc - 1) / 2;
+    num_players = n;
+    players = (bot_player *)malloc(sizeof(bot_player) * n);
+
+    int i;
+    for (i = 0; i < n; i++)
+        players[i] = bot_player_create(argv[1 + i], argv[1 + i + n]);
+
+    return n;
 }
 
-void child_send(childproc proc, const char *str)
+void bot_player_finish()
 {
-    bot_send_blob(proc.fd_send, 0, str);
+    int i;
+    for (i = 0; i < num_players; i++) {
+        fsync(players[i].fd_log);
+        kill(players[i].pid, SIGTERM);
+        while (waitpid(players[i].pid, 0, 0) > 0) { }
+    }
 }
 
-char *child_recv(childproc proc, size_t *o_len, int timeout)
+void bot_player_send(int id, const char *str)
 {
-    child_resume(proc);
-    char *resp = bot_recv_blob(proc.fd_recv, o_len, timeout);
-    child_pause(proc);
+    bot_send_blob(players[id].fd_send, 0, str);
+}
+
+char *bot_player_recv(int id, int *o_len, int timeout)
+{
+    size_t len;
+    bot_player_resume(players[id]);
+    char *resp = bot_recv_blob(players[id].fd_recv, &len, timeout);
+    bot_player_pause(players[id]);
+    if (o_len != NULL) *o_len = len;
     return resp;
 }
 
